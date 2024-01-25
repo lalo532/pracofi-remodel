30cc9a00deff5a5bcbbab0b201acc9a756508c4a
Alexei Palacios <sukesasuk2010@gmail.com>
2024-01-25 14:45:27 -0600

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomeServicesComponent } from './components/home-services/home-services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'navbar', component: NavbarComponent},
+  { path: 'landing', component: LandingPageComponent},    
+  { path: 'login', component: LoginComponent},
+  { path: '',   redirectTo: '/landing', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeServicesComponent } from './components/home-services/home-services.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GuaranteeComponent } from './components/guarantee/guarantee.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ServicesPageComponent } from './pages/services-page/services-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeServicesComponent,
+    FooterComponent,
+    GuaranteeComponent,
+    LandingPageComponent,
+    ServicesPageComponent
   ],
   imports: [
     BrowserModule,
